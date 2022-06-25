dd075477b2f9428de07ce9b9a98a4f731e635185
TraktorT330 <sport11b@ya.ru>
2022-06-25 19:46:55 +0300

    выполнение 1го задания 3го урока

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,20 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+export   function createGetter(path) {
+  const pathArray  = path.split('.'); // получаем в pathArray массив имен из пути
+  return (obj) => {
+      const handler = (acc, curr, index) =>
+      {
+        if (acc !== undefined){
+          const res = acc[curr];
+          console.log('res',res);
+          console.log('curr',curr);
+          console.log('acc',acc);
+          return res
+        }};
 
+    const res = pathArray.reduce(handler,obj)
+    return res;
+  }
 }
