a4850eb186ebbd5aa00637940680a6d5b267c13b
TraktorT330 <sport11b@ya.ru>
2022-06-26 15:20:50 +0300

    выполнение 2го задания 3го урока

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -3,6 +3,19 @@
  * @param {object} obj - the initial object
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
-export function invertObj(obj) {
+export   function invertObj(obj) {
+  if (obj!== undefined) {
+    const ar = Object.entries(obj)
+    const handler = (acc, curr, index) => {
+      console.log('index', index);
+      console.log('curr1', curr[0]);
+      console.log('curr2', curr[1]);
+      acc[curr[1]] = curr[0];
+      console.log('acc', acc);
+      return acc
+    };
 
+    const res = ar.reduce(handler, {})
+    return res
+  } else return undefined
 }
