39e2b100c5eda4c82f4e6d001d11c0c5a811e614
TraktorT330 <sport11b@ya.ru>
2022-06-26 19:48:01 +0300

    выполнение 3го задания 3го урока

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,28 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export function trimSymbols(string, size)
+{
 
+  if (size === 0) return '';
+  if (size === undefined) return string;
+
+  let ar = Array.from(string);
+  let first_slice = ar.slice(0,size).join('');
+  let rest_slice = ar.slice(size,);
+  console.log('first_slice', first_slice)
+  console.log('rest_slice', rest_slice)
+
+  const handler = (acc,curr,index) =>
+  {
+
+    if (!acc.endsWith(curr.repeat(size))) {acc = acc + curr} else {acc};
+    console.log('index', index);
+    console.log('curr', curr);
+    console.log('acc', acc);
+    return acc
+  }
+
+  const res = ar.reduce(handler,first_slice)
+  return res
 }
